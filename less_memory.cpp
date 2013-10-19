ab83abb0da1e7182c5f317611369791349336163
Eugene Chen <echen@avmathteam.org>
2013-10-19 16:06:55 -0400

    Figured out how to use long double...
    printf("%.9Lf\n", ans)

--- less_memory.cpp
+++ less_memory.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 const int MAX_DEPTH = 100000; // at MAX_DEPTH flips you are forced to quit
-const double P = 0.5; // probability of heads
+const long double P = 0.5; // probability of heads
 
-double value[MAX_DEPTH + 1];
+long double value[MAX_DEPTH + 1];
 
-double ans;
+long double ans;
 
-double divide(int a, int b)
+long double divide(int a, int b)
 {
 	return a / (b + 0.0);
 }
@@ -19,7 +19,7 @@ void compute(int depth)
 {
 	for (int h = 0; h < depth + 1; h++)
 	{
-		value[h] = max(divide(h, depth), (double) 0.5);
+		value[h] = max(divide(h, depth), (long double) 0.5);
 	}
 
 	for (int total = depth - 1; total >= 1; total--)
@@ -36,9 +36,11 @@ void compute(int depth)
 		}
 	}
 
-	double ans = (1 - P) * value[0] + P * value[1];
+	long double ans = (1 - P) * value[0] + P * value[1];
 
-	cout << ans << endl;
+	printf("%.12Lf\n", ans);
+
+	// cout << ans << endl;
 }
 
 int main()
